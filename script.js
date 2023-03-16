d8b1be05eb0ccd8093cfb714d48c5be0664ce5b4
Jean Ferreira <86920715+Jeanikt@users.noreply.github.com>
2023-03-15 23:07:12 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function randomNumber(min, max){
+    return Math.floor(Math.random() * (max - min + 1))
+}
+const STAR_COUNT = 100
+let result = " "
+for(let i = 0; i < STAR_COUNT; i++){
+    result += `${randomNumber(-50, 50)}vh ${randomNumber(0, 3)}px ${randomNumber(0, 3)}px #fff,`
+}
+console.log(result.substring(0, result.length - 1))
+
+const year = document.querySelector('#current-year')
+year.innerHTML = new Date().getFullYear()
